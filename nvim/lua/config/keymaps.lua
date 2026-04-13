242668da90e5d5fc1c2eba6b0ab606aacf073ef0
Ivan Danov <idanov@users.noreply.github.com>
2026-04-14 00:15:53 +0100

    Add tmux navigation settings to LazyVim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,25 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- Keep pane navigation mappings here instead of the plugin spec. In LazyVim,
+-- lazy plugin key declarations for vim-tmux-navigator can be unreliable.
+-- Defining them here makes Alt-h/j/k/l available consistently at startup.
+
+-- Normal-mode pane navigation across Neovim splits and tmux panes.
+vim.keymap.set("n", "<M-h>", "<Cmd>TmuxNavigateLeft<CR>", { desc = "Navigate h" })
+vim.keymap.set("n", "<M-j>", "<Cmd>TmuxNavigateDown<CR>", { desc = "Navigate j" })
+vim.keymap.set("n", "<M-k>", "<Cmd>TmuxNavigateUp<CR>", { desc = "Navigate k" })
+vim.keymap.set("n", "<M-l>", "<Cmd>TmuxNavigateRight<CR>", { desc = "Navigate l" })
+
+-- Insert mode must leave insert before changing windows.
+vim.keymap.set("i", "<M-h>", "<Esc><Cmd>TmuxNavigateLeft<CR>", { desc = "Navigate h" })
+vim.keymap.set("i", "<M-j>", "<Esc><Cmd>TmuxNavigateDown<CR>", { desc = "Navigate j" })
+vim.keymap.set("i", "<M-k>", "<Esc><Cmd>TmuxNavigateUp<CR>", { desc = "Navigate k" })
+vim.keymap.set("i", "<M-l>", "<Esc><Cmd>TmuxNavigateRight<CR>", { desc = "Navigate l" })
+
+-- Terminal mode must leave terminal-input mode before switching panes.
+vim.keymap.set("t", "<M-h>", [[<C-\><C-n><Cmd>TmuxNavigateLeft<CR>]], { desc = "Navigate h" })
+vim.keymap.set("t", "<M-j>", [[<C-\><C-n><Cmd>TmuxNavigateDown<CR>]], { desc = "Navigate j" })
+vim.keymap.set("t", "<M-k>", [[<C-\><C-n><Cmd>TmuxNavigateUp<CR>]], { desc = "Navigate k" })
+vim.keymap.set("t", "<M-l>", [[<C-\><C-n><Cmd>TmuxNavigateRight<CR>]], { desc = "Navigate l" })
---
+++ nvim/lua/plugins/tmux-navigator.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1
+      vim.g.tmux_navigator_save_on_switch = 1
+    end,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+  },
+}
